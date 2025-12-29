e484ccf13f01ec4c1ae6f55787f6d9bf0ad2b6a0
unknown <krpandey1106@gmail.com>
2025-12-29 11:18:31 +0530

    Expose is_superuser in add user form

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,18 +5,34 @@ from .models import CustomUser
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ("username", "email", "is_staff", "is_active", "date_joined", "is_superuser")
-    list_filter = ("is_staff", "is_active", "groups")
+    list_display = (
+        "username", "email", "is_staff", "is_active", "is_superuser", "date_joined"
+    )
+    list_filter = ("is_staff", "is_active", "is_superuser", "groups")
     search_fields = ("username", "email")
     ordering = ("-date_joined",)
+
+    # Fields shown when editing an existing user
     fieldsets = (
         (None, {"fields": ("username", "email", "password")}),
-        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+        ("Permissions", {
+            "fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")
+        }),
         ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
+
+    # Fields shown when adding a new user
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
-            "fields": ("username", "email", "password1", "password2", "is_staff", "is_active"),
+            "fields": (
+                "username",
+                "email",
+                "password1",
+                "password2",
+                "is_staff",
+                "is_active",
+                "is_superuser",   
+            ),
         }),
     )
